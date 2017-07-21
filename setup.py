9172638d074e86b7b9dc506c2ce3bb1755ae581e
Brian <brian@quantrocket.com>
2017-07-21 15:55:43 -0400

    remove README.md from setup.py because only .rst is supported

--- setup.py
+++ setup.py
@@ -3,16 +3,11 @@
 from setuptools import setup
 import versioneer
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 setup(
     name='ibopt',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='IbPy-like interface for the Interactive Brokers Python API',
-    long_description=readme(),
     maintainer='QuantRocket LLC',
     maintainer_email='support@quantrocket.com',
     url='https://github.com/quantrocket-llc/ibopt',
